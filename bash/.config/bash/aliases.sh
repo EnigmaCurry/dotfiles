250320fa04173bdc5150e753eb0850942c4979d5
Ryan McGuire <ryan@enigmacurry.com>
2016-04-22 20:04:49 -0400

    aliases and agents

--- bash/.config/bash/aliases.sh
+++ bash/.config/bash/aliases.sh
@@ -1,4 +1,5 @@
 alias ls='ls --color=auto'
 alias ssh_anon="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 alias forever="yes | xargs -I{} -P1"
-alias ec="emacsclient"
+alias ec="emacsclient -a ''"
+alias sudo_ec="SUDO_EDITOR=\"emacsclient -a ''\" sudoedit"
